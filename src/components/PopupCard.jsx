aab85447477321da1d08414eb62caace5c4c7374
hiidev <cadetolati@gmail.com>
2025-07-04 01:24:08 -1100

    updating popuup ref

--- src/components/PopupCard.jsx
+++ src/components/PopupCard.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import '../styles/PopupCard.css'
+import { TbClockHour4 } from "react-icons/tb";
+import { IoPlayCircleOutline } from "react-icons/io5";
+import Rating from './Rating';
 
 const PopupCard = ({item, position, onClose}) => {
+
+  const closeRef = useRef();
+  useEffect(()=>{
+    const handleClose = ()=>{
+      if(closeRef.current && !closeRef.current.contains(e.traget)){
+        onClose();
+      }
+    };
+    document.addEventListener('mousedown', handleClose);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClose)
+    }
+  }, [onClose]);
+
 
   if(!item) return null
   return (
@@ -10,9 +28,10 @@ const PopupCard = ({item, position, onClose}) => {
         {top: position.y, 
          left: position.x, 
          position: "absolute", 
-         zIndex: 99}
+         zIndex: 2}
          }
-         className='popupCard'
+      className='popupCard'
+      ref={closeRef}
     >
       <div className='popupHeadCon'>
         <div className='popupImgCon'><img src={item.thumbnail} alt={item.title} /></div>
@@ -21,6 +40,28 @@ const PopupCard = ({item, position, onClose}) => {
           <p>{item.numberOfLessons}  videos</p>
         </div>
       </div>
+      <p className='popupIntro'>
+        Learn the fundamentals of working 
+        with {item.heading} and how to create basic applications.
+      </p>
+
+      <div className='popupMain'>
+        <div className='popupIconCon'>
+          <p>
+            <span><TbClockHour4 size={16}/></span>
+             {item.hours} hours
+          </p>
+          <p>
+            <span><IoPlayCircleOutline size={16}/></span>
+             {item.numberOfLessons} courses
+          </p>
+        </div>
+        <div className='popupBtnCon'>
+          <button>resume</button>
+          <button>start over</button>
+        </div>
+      </div>
+      <p className='cRating'> <span>course rating </span><Rating rating={item.rating} style={{color: 'var(--main)'}}/></p>
     </div>
   )
 }
